21b7df8109f85eec3998c2af2c8e44b533dbde0b
Nagabtac <darwin.catbagan@lorma.edu>
2025-01-15 09:02:56 +0800

    Second Commit

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class HelloWorld{
+    public static void main(String []args){
+
+     Scanner input = new Scanner(System.in);
+        /*DIs is just for me to see if my new repo is working well */
+        System.out.println("Add extra message? type [y] for yes or [n] for no");
+        String answer = input.nextLine();
+
+        System.out.println("Write extra message: ");
+        String extraMessage = input.nextLine();
+
+        if(answer.toLowerCase()== "y"){
+
+        System.out.println("Hello world! " + extraMessage);
+        }else{
+            System.out.println("Helloworld! ");
+        }
+        input.close();
+        
+
+
+
+        
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+# This Repo is my second and is testing again if it really works
+# dont mind this please 
