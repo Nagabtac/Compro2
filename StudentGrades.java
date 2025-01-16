6d19cf15a86cfaba53cbd7cfc7d015c08eedea5e
Nagabtac <darwin.catbagan@lorma.edu>
2025-01-16 12:28:16 +0800

    made html file and updated studentgrades file

--- StudentGrades.java
+++ StudentGrades.java
@@ -1,4 +1,14 @@
 public class StudentGrades {
+    public static void main (String []args){
     
-    
+    int count = 1;
+    while(true){
+        //student 1
+        p("Processing grade for student " + count + ": ");
+
+        String [] subjects = {"Science", "Math"}
+
+    }
+    }
+  
 }
