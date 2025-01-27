417266f5a74b1cd6e5ff982f81cfb8891a977c70
Nagabtac <darwin.catbagan@lorma.edu>
2025-01-27 09:27:12 +0800

    coffeorder update

--- kapeNaNakakaTulog.java
+++ kapeNaNakakaTulog.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class kapeNaNakakaTulog {
+    public static final  String coffeeMenu = String.format("""
+        ---Coffee Menu---
+       1. Espresso        - 50.0 PHP
+       2. Latte           - 70.0 PHP
+       3. Cappuccino      - 65.0 PHP
+       4. Mocha           - 80.0 PHP
+       0. Finish Order
+       """);
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -11,35 +21,27 @@ public class kapeNaNakakaTulog {
         double grandTotal = 0; 
 
         loop: for (int i = 0; i < 5; i++) { 
-            String coffeeMenu = String.format("""
-             ---Coffee Menu---
-            1. Espresso        - 50.0 PHP
-            2. Latte           - 70.0 PHP
-            3. Cappuccino      - 65.0 PHP
-            4. Mocha           - 80.0 PHP
-            0. Finish Order
-            """);
             System.out.print(coffeeMenu);
 
             System.out.print("Choose Your Coffee (1-4) or 0 to finish: ");
             order[i] = input.nextInt();
+            
 
             if (order[i] > 4 || order[i] < 0) {
                 System.out.println("Invalid choice. Please try again.");
-                i--;
                 continue;
             }
 
-            int selectedItem = order[i];
+            //int selectedItem = order[i];
 
-            if (selectedItem == 0) {
+            if (order[i] == 0) {
                 break loop; 
             }
 
             System.out.print("Enter Quantity: ");
             quantity[i] = input.nextDouble(); 
 
-            switch (selectedItem) {
+            switch (order[i]) {
                 case 1:
                     subtotal += 50 * quantity[i];  
                     break;
@@ -55,6 +57,7 @@ public class kapeNaNakakaTulog {
             }
         }
 
+
         vat = subtotal * 0.12;  
         grandTotal = subtotal + vat;  
 
@@ -66,11 +69,21 @@ public class kapeNaNakakaTulog {
              ---------------------
         """, subtotal, vat, grandTotal);
 
+
         System.out.println(receipt);
-        
-        System.out.println(receipt);
+
+        try (FileWriter writer = new FileWriter("CoffeeReceipt.txt")) {
+            writer.write("how to print coffeeMenu");
+            writer.write(receipt);
+            writer.write("\nThis is a file writing example.");
+            System.out.println("Data successfully written to file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+        //use 2d array?!?
 
 
         input.close();
     }
+    
 }
