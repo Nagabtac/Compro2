baa0190ddef2bca33ae074569a8449a443e9592e
Nagabtac <darwin.catbagan@lorma.edu>
2025-02-04 21:18:14 +0800

    ok

--- coffee.java
+++ coffee.java
@@ -1,6 +1,19 @@
-public class Objects {
-    public static void main(String[] args) {
-        System.out.println("Object");
+public class coffee {
+    // Fields (state)
+    String color;
+    String model;
+    int speed;
+
+    // Methods (behavior)
+    void accelerate() {
+        speed += 10;
     }
-    
+
+    void brake() {
+        speed -= 10;
+    }
+
+    void honk() {
+        System.out.println("Beep beep!");
+    }
 }
